6f15f35cd23a558d82171d7dc9684b7c63168165
RMOATMAN <raemarie.oatman@gmail.com>
2021-08-10 22:51:08 -0400

    updated chat navigation

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -14,28 +14,7 @@ const dashboardFormHandler = (event) => {
     window.location.replace("/dashboard");
 };
         
-    // Listens for Home button to be selected
-    document
-    .querySelector('#dashboard-button')
-    .addEventListener('click', dashboardFormHandler);
-
-
-    // Action taken when Logout button is selected
-// const logout = async () => {
-//     const response = await fetch('/api/users/logout', {
-//       method: 'POST',
-//       headers: { 'Content-Type': 'application/json' },
-//     });
-  
-//     // If successfully logged out, redirect user to the login page
-//     if (response.ok) {
-//       document.location.replace('/login');
-//     } else {
-//       alert(response.statusText);
-//     }
-//   };
-  
-//   // Listens for user to select Logout
-//     document.querySelector('#logout')
-//     .addEventListener('click', logout);
-  
+// Listens for Delete button to be selected
+document
+.querySelector('#dashboard-button')
+.addEventListener('click', dashboardFormHandler);
